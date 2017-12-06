27ced9d7c8312efd182683ac545fcf74ac88379a
sakazawasaburo <keitaro_nishizawa@gyosei-system.co.jp>
2017-12-06 11:01:02 +0900

    s

---
+++ src/main/java/com/example/sample/LoginUserDetails.java
@@ -0,0 +1,20 @@
+/*
+package com.example.demo;
+
+import lombok.Data;
+
+@SuppressWarnings("serial")
+@Data
+public class LoginUserDetails extends org.springframework.security.core.userdetails.User{
+	private final LoginUser loginUser;
+	
+	public LoginUserDetails(LoginUser loginUser) {
+		super(loginUser.getUserid(), loginUser.getPassword(), loginUser.getAuth());
+		this.loginUser = loginUser;
+	}
+
+	public LoginUser getLoginUser() {
+		return loginUser;
+	}
+}
+*/
